38783a2c0ea1a33547f5e6cd77943eb3288b615d
Sergey207 <67117006+Sergey207@users.noreply.github.com>
2021-11-14 18:41:59 +0300

    Календарь-ежедневник, который находится в автозапуске и работает в фоне и напоминает о событиях. События делятся на постоянные и переменные. Также присутствует функция сохранение месяца в excel файл для печати.
    ***
    
    В приложении FamilyCalendar.exe находятся все функции приложения - добавление, удаление,
    изменение членов семьи. А также добавление событий и функция преобразования текущего
    месяца в файл excel.
    
    ***
    
    Приложение eventer.exe работает в фоне и напоминает о событиях.

--- DialogsDesigns/CustomCalendar.py
+++ DialogsDesigns/CustomCalendar.py
@@ -1,18 +1,23 @@
-from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QRect, QDate
+from PyQt5.QtGui import QColor, QPainter, QFont
 from PyQt5.QtWidgets import QCalendarWidget
 
 
 class CustomCalendar(QCalendarWidget):
     def __init__(self, parent=None):
         QCalendarWidget.__init__(self, parent)
+        self.grayColor, self.blackColor = QColor(150, 150, 150), QColor(0, 0, 0)
         self.setGridVisible(True)
         self.setVerticalHeaderFormat(QCalendarWidget.NoVerticalHeader)
         self.setStyleSheet('''font-size: 18px;''')
 
-    def paintCell(self, painter, rect, date):
-        # QCalendarWidget.paintCell(self, painter, rect, date)
-        print(date)
-        painter.drawText(rect,0, str(date.day()))
+    def paintCell(self, painter: QPainter, rect: QRect, date: QDate):
+        print('Drawing cell...')
+        if self.monthShown() == date.month():
+            painter.setPen(self.blackColor)
+        else:
+            painter.setPen(self.grayColor)
+        painter.drawText(rect, 0, str(date.day()))
         if date == date.currentDate():
             painter.setBrush(QColor(178, 236, 93, 150))
             painter.drawRect(rect)
