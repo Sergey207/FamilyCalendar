bab2fc178c281469213cbf57c72de056ce0271cf
Sergey207 <67117006+Sergey207@users.noreply.github.com>
2021-11-20 19:22:10 +0300

    https://trello.com/b/eMfPBVX5/familycalendar
    Добавлена частичная поддержка регулярных событий

--- DialogsDesigns/CustomCalendar.py
+++ DialogsDesigns/CustomCalendar.py
@@ -1,5 +1,5 @@
 import datetime
-import pprint
+from pprint import pprint
 import sqlite3
 
 from PyQt5.QtCore import QRect, QDate
@@ -24,33 +24,6 @@ class CustomCalendar(QCalendarWidget):
     def repaint(self):
         self.updateDB()
         QCalendarWidget.repaint(self)
-
-    def updateDB(self):
-        self.colors = {i[0]: (i[1], i[2], i[3]) for i in self.cur.execute('select * from colors')}
-        self.typesOfRegular = {i[0]: i[1] for i in
-                               self.cur.execute('select id, title from typesOfRegular')}
-        self.familyMembers = {i[0]: self.colors[i[1]] for i in
-                              self.cur.execute('select id, color from familyMembers')}
-
-        pprint.pprint(self.typesOfRegular)
-        self.events = []
-        for idOfType, title in self.typesOfRegular.items():
-            for i in self.cur.execute(f'''select * from events where typeOfRegular = {idOfType}'''):
-                date = QDate(int(i[4].split('.')[2]), int(i[4].split('.')[1]),
-                             int(i[4].split('.')[0]))
-                self.events.append((self.familyMembers[i[0]], i[1], i[2],
-                                    date))
-                if i[1] == 1:
-                    minMonthNumber = self.monthShown() - 1 if self.monthShown() > 1 else 12
-                    maxMonthNumber = self.monthShown() + 1 if self.monthShown() < 12 else 1
-                    for monthNumber in (minMonthNumber, self.monthShown(), maxMonthNumber):
-                        for dayNumber in range(1, 32):
-                            print(monthNumber, '-', dayNumber, sep='')
-                            if QDate(int(i[4].split('.')[2]), monthNumber, dayNumber) >= date:
-                                self.events.append((self.familyMembers[i[0]], i[1], i[2],
-                                                    QDate(int(i[4].split('.')[2]), monthNumber,
-                                                          dayNumber)))
-        print(self.events)
 
     def paintCell(self, painter: QPainter, rect: QRect, date: QDate):
         new_line_simbols = ''
@@ -86,3 +59,39 @@ class CustomCalendar(QCalendarWidget):
                 painter.drawRect(self.todayRect)
         except BaseException as e:
             print(e)
+
+    def updateDB(self):
+        self.colors = {i[0]: (i[1], i[2], i[3]) for i in self.cur.execute('select * from colors')}
+        self.typesOfRegular = {i[0]: i[1] for i in
+                               self.cur.execute('select id, title from typesOfRegular')}
+        self.familyMembers = {i[0]: self.colors[i[1]] for i in
+                              self.cur.execute('select id, color from familyMembers')}
+
+        pprint(self.typesOfRegular)
+        self.events = []
+        for idOfType, title in self.typesOfRegular.items():
+            for i in self.cur.execute(f'''select * from events where typeOfRegular = {idOfType}'''):
+                date = QDate(int(i[4].split('.')[2]), int(i[4].split('.')[1]),
+                             int(i[4].split('.')[0]))
+                minMonthNumber = self.monthShown() - 1 if self.monthShown() > 1 else 12
+                maxMonthNumber = self.monthShown() + 1 if self.monthShown() < 12 else 1
+                if i[1] == 1:
+                    for monthNumber in (minMonthNumber, self.monthShown(), maxMonthNumber):
+                        for dayNumber in range(1, 32):
+                            if self.monthShown() <= 1 and monthNumber == 12:
+                                year = self.yearShown() - 1
+                            elif self.monthShown() >= 12 and monthNumber == 1:
+                                year = self.yearShown() + 1
+                            else:
+                                year = self.yearShown()
+                            if QDate(year, monthNumber, dayNumber) >= date:
+                                self.events.append((self.familyMembers[i[0]], i[1], i[2],
+                                                    QDate(year, monthNumber, dayNumber)))
+                elif i[1] == 2:
+                    while date.month() in (minMonthNumber, self.monthShown(), maxMonthNumber):
+                        self.events.append((self.familyMembers[i[0]], i[1], i[2],
+                                            date))
+                        date.addDays(2)
+                elif i[1] == 10:
+                    self.events.append((self.familyMembers[i[0]], i[1], i[2],
+                                        date))
